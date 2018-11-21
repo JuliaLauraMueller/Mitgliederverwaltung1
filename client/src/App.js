aa9ad138a85d917ee5a82c529ea0a248502bd34d
Dominik Bumbacher <dominik.bumbacher@students.fhnw.ch>
2018-11-21 18:40:18 +0100

    Created a first login page using react VT341805-72

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+// Components
 import React, { Component } from 'react';
+import './App.css';
+import Router from './Router';
+import { Container } from 'reactstrap';
+
+// Styles
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class App extends Component {
+  // Add global layout components before route
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <Container style={{marginTop: '5rem'}}>
+          <Router />
+        </Container>
       </div>
     );
   }
