bc2baecd5e9fde4592add38fd9fcc6f984509300
unknown <tabea.eggler@hotmail.com>
2018-11-26 11:57:12 +0100

    VT341805-78: basic structur of ProfilePage

--- client/src/Router.js
+++ client/src/Router.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 // Import pages
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
 
 class Router extends Component{
     render() {
@@ -12,6 +13,7 @@ class Router extends Component{
             <Switch>
                 <Route exact path="/" component={HomePage} />
                 <Route exact path="/login" component={LoginPage} />
+                <Route exact path="/profile" component={ProfilePage} />
             </Switch>
         );
     }
