2b892989db59d9c380686c65242057d59ac3787a
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2018-11-27 15:01:29 +0100

    VT341805-79: Added css and card imput now reads from an array

--- client/src/components/MemberCard.js
+++ client/src/components/MemberCard.js
@@ -1,56 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, View } from 'react';
 import { Card, CardImg, CardText, CardBody,
     CardTitle, Container, Row, Col, CardColumns } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import '../cssx/MemberCSS.css';
   
-class MemberCard extends Component{
+class MemberCard extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
     render() {
+
         return (
-        <div>
-            <CardColumns>
+            <div>
                 <Card className="member-card" style={{ border: "1px solid white" }}>
-                <CardBody>
-                    <Container>
-                        <Row className="row-title">
-                            <Col sm="5"><img className="card-image" src={ require('../img/marc_zimmermann.jpg') } alt="Card i cap" /></Col>
-                            <Col><CardTitle className="card-name">Marc <br></br>Zimmermann</CardTitle></Col>
-                        </Row>
-                        <Row className="row-text"> 
-                            <Col sm="2"><CardImg src={ require('../img/marc_zimmermann.jpg') } alt="Card image cap" /><CardText className="card-text">test@mail.ch</CardText></Col>
-                    
-                        </Row>
-                        <Row>
-                            <Col sm="2"><CardImg src={ require('../img/marc_zimmermann.jpg') } alt="Card image cap" /><CardText className="card-text">0791234567</CardText></Col>
-                        </Row>
-                        <Row>
-                            <Col><CardText className="profile-text">Profil></CardText></Col>
-                        </Row>
-                    </Container>
-                </CardBody>
+                    <CardBody>
+                        <Container>
+                            <Row className="row-title">
+                                <Col sm="5"><img className="card-image" src={this.props.member.profilepic} alt="Card i cap"/></Col>
+                                <Col><CardTitle className="card-name">{this.props.member.prename} <br></br>{this.props.member.lastname}</CardTitle></Col>
+                            </Row>
+                            <Row className="row-text">
+                                <Col sm="2"><img className="mail-icon" src={require('../img/mail.png')} alt="Card i cap" /><CardText className="card-text">{this.props.member.mail}</CardText></Col>
+
+                            </Row>
+                            <Row>
+                                <Col sm="2"><img className="phone-icon" src={require('../img/phone.png')} alt="Card i cap" /><CardText className="card-text">{this.props.member.phone}</CardText></Col>
+                            </Row>
+                            <Row>
+                                <Col><CardText><Link className="profile-text" to="/login">Profil></Link></CardText></Col>
+                            </Row>
+                        </Container>
+                    </CardBody>
                 </Card>
 
-                <Card className="member-card" style={{ border: "1px solid white" }}>
-                <CardBody>
-                    <Container>
-                        <Row className="row-title">
-                            <Col sm="5"><img className="card-image" src={ require('../img/marc_zimmermann.jpg') } alt="Card i cap" /></Col>
-                            <Col><CardTitle className="card-name">Marc <br></br>Zimmermann</CardTitle></Col>
-                        </Row>
-                        <Row className="row-text"> 
-                            <Col sm="2"><CardImg src={ require('../img/marc_zimmermann.jpg') } alt="Card image cap" /><CardText className="card-text">test@mail.ch</CardText></Col>
-                    
-                        </Row>
-                        <Row>
-                            <Col sm="2"><CardImg src={ require('../img/marc_zimmermann.jpg') } alt="Card image cap" /><CardText className="card-text">0791234567</CardText></Col>
-                        </Row>
-                        <Row>
-                            <Col><CardText className="profile-text">Profil></CardText></Col>
-                        </Row>
-                    </Container>
-                </CardBody>
-                </Card>
-        </CardColumns>
-        </div>
+            </div>
         );
     };
 }
--- client/src/pages/MemberPage.js
+++ client/src/pages/MemberPage.js
@@ -1,14 +1,82 @@
 import React, { Component } from 'react';
 import MemberCard from '../components/MemberCard';
 import '../cssx/MemberCSS.css';
+import {Helmet} from "react-helmet";
+import { Container, Row } from 'reactstrap';
+
 
 class MemberPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            members: [
+                {
+                prename: "Steven",
+                lastname: "Wüthrich",
+                mail: "sw@fatpanda.io",
+                phone: "07923456789",
+                profilepic: require('../img/marc_zimmermann.jpg')
+                },
+                
+                {
+                prename: "Marc",
+                lastname: "Zimmermann",
+                mail: "mz@fatpanda.io",
+                phone: "07925456789",
+                profilepic: require('../img/marc_zimmermann.jpg')
+                },
+
+                {
+                prename: "Renato",
+                lastname: "Gnocchi",
+                mail: "rg@fatpanda.io",
+                phone: "07923692789",
+                profilepic: require('../img/marc_zimmermann.jpg')
+                },
+
+                {
+                prename: "Steven",
+                lastname: "Wüthrich",
+                mail: "sw@fatpanda.io",
+                phone: "07923456789",
+                profilepic: require('../img/marc_zimmermann.jpg')
+                },
+                
+                {
+                prename: "Marc",
+                lastname: "Zimmermann",
+                mail: "mz@fatpanda.io",
+                phone: "07925456789",
+                profilepic: require('../img/marc_zimmermann.jpg')
+                }
+            ]
+        }
+    }
     render() {
-        return (   
-                <div>
+        let memberCards = this.state.members.map(member => {
+            return (
+                <MemberCard member={member}></MemberCard>
+            )
+        })
+        return (
+        <div>
+            <div>
+                <Helmet>
+                    <style>{'body { background-color: rgb(15, 25, 41, 10%); }'}</style>
+                </Helmet>
+            </div>
+
+            <div>             
                 <h1>Members</h1>
-                <MemberCard></MemberCard>
-                </div>
+                <Container fluid>
+                    <Row>
+                        {memberCards}
+                    </Row>
+                </Container>
+            </div>
+        </div>
+              
+                
         );
     }
 }
