4007edd4f3b4e03d8afef55248e56421df24bf9e
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2018-11-29 11:24:40 +0100

    VT341805-79: Fixed profile link, added svg picture of arrow

--- client/src/components/MemberCard.js
+++ client/src/components/MemberCard.js
@@ -25,24 +25,21 @@ class MemberCard extends Component {
                 <Card className="member-card" style={{ border: "1px solid white" }}>
                     <CardBody>
                         <Container>
-                            <Row className="row-title">
-                                <Col xs="auto"><img className="card-image" src={this.props.member.profilepic} alt="Card i cap"/></Col>
-                                <Col><CardTitle className="card-name">{this.props.member.prename} <br></br>{this.props.member.lastname}</CardTitle></Col>
-                            </Row>
-                            <Row className="row-text">
-                                <Col><img className="mail-icon" src={require('../img/mail.png')} alt="Card i cap" />
-                                <CardTitle className="card-text-mail">{this.props.member.mail}</CardTitle></Col>
-
-                            </Row>
-                            <Row>
-                                <Col><img className="phone-icon" src={require('../img/phone.png')} alt="Card i cap" />
-                                <CardTitle className="card-text-phone">{this.props.member.phone}</CardTitle></Col>
-                            </Row>
-                            <Row>
-                                
-                                    <Link className="profile-text" to="/login">Profil></Link>    
-                                
-                            </Row>
+                            <img className="card-image" src={this.props.member.profilepic} alt="Card i cap"/>
+                            <CardTitle className="card-name">{this.props.member.prename} <br></br>{this.props.member.lastname}</CardTitle>
+                        </Container>
+                        <Container>
+                            <img className="mail-icon" src={require('../img/mail.png')} alt="Card i cap" />
+                            <CardText className="card-text-mail">{this.props.member.mail}</CardText>   
+                        </Container>
+                        <Container>
+                            <img className="phone-icon" src={require('../img/phone.png')} alt="Card i cap" />
+                            <CardTitle className="card-text-phone">{this.props.member.phone}</CardTitle>
+                        </Container>
+                        <Container className="profile-link">
+                            <div className="profile-div">
+                                <Link className="profile-text" to="/login">Profil <img className="profile-arrow"src={require('../img/arrow.svg')} alt="Card i cap"></img></Link>
+                            </div> 
                         </Container>
                     </CardBody>
                 </Card>
