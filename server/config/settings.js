b6982e96bbd8e6052d8df4c21636b87f86ee77b4
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2018-12-16 21:32:27 +0100

    VT341805-101: changed jwt expiration time to 15 minutes

--- server/config/settings.js
+++ server/config/settings.js
@@ -2,5 +2,5 @@ module.exports = {
   mongoURI:
     'mongodb://mitgliederadm:smallfishswimsfast@86.119.36.153:27017/mitgliederverwaltung',
   jwtSecret: 'sweetcatslikechocolate',
-  jwtExpirationSeconds: 5 // 15min
+  jwtExpirationSeconds: 60 * 15 // 15min
 };
