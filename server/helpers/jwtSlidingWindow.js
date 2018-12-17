15dccfe99e1ff83c6cec7e2080d393358eb493a7
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2018-12-17 19:37:29 +0100

    VT341805-101: only update token after half of the expiration time

--- server/helpers/jwtSlidingWindow.js
+++ server/helpers/jwtSlidingWindow.js
@@ -1,5 +1,6 @@
 const jwtDecode = require('jwt-decode');
 const userService = require('../services/userService');
+const config = require('../config/settings');
 
 module.exports = addUpdatedTokenToHeader;
 
@@ -8,8 +9,15 @@ function addUpdatedTokenToHeader(req, res, next) {
     !(req.originalUrl == '/api/users/auth' || req.originalUrl == '/api/login')
   ) {
     const decodedToken = jwtDecode(req.get('Authorization'));
-    const newToken = userService.generateJwtToken(decodedToken);
-    res.append('Set-Authorization', newToken);
+    const currTime = new Date().getTime() / 1000;
+    // only send updated token if at least half the expiration duration of the current token has already passed
+    if (
+      decodedToken &&
+      decodedToken.exp - currTime < config.jwtExpirationSeconds / 2
+    ) {
+      const newToken = userService.generateJwtToken(decodedToken);
+      res.append('Set-Authorization', newToken);
+    }
   }
   next();
 }
