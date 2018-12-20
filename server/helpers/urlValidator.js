9e81e421ac8a62cb38a246d3d8ba124af2b53a06
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2018-12-20 10:53:41 +0100

    VT341805-101: changes after feedback

--- server/helpers/urlValidator.js
+++ server/helpers/urlValidator.js
@@ -6,9 +6,6 @@ function validateUrl(url) {
     url.includes('javascript:') // eslint-disable-line no-script-url
   ) {
     url = '';
-  } else if (!url.startsWith('http')) {
-    url = 'http://' + url;
   }
-  console.log(url);
   return url;
 }
