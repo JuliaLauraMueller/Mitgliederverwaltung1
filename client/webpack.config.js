5feee83904155771fa1e6165cffd5931012a7535
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2018-12-26 18:15:23 +0100

    VT341805-297: reverted last change, "copy-webpack-plugin" dependency is needed but was missing in package-lock.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
