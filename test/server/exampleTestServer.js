d53cac29322e5539b30059099595665eb4db65f9
Robin Schuerch <robin.schuerch@students.fhnw.ch>
2019-02-28 13:26:02 +0100

    VT341805-195: Added sample serverside test with backend integration

--- test/server/exampleTestServer.js
+++ test/server/exampleTestServer.js
@@ -1,14 +1,21 @@
 var assert = require('assert');
+var userService = require('../../server/services/userService');
+
 describe('ExampleServerSideTest', function() {
   describe('ServerSideTest()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+    it('is this something', function() {
+      var data = userService.getAll();
+      if (data) {
+        assert.equal(true, true);
+      } else {
+        assert.equal(false, true);
+      }
     });
   });
 
   describe('ServerSideTest2()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+      assert.equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
