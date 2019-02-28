5caeaf7bf445cfe8b6b8b743d3aea8515c5bbf3a
Dominik Bumbacher <dominik.bumbacher@students.fhnw.ch>
2019-02-28 18:25:12 +0100

    Member search bar centering

--- client/src/components/SearchFieldMember.js
+++ client/src/components/SearchFieldMember.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import { Container, Row, Col } from "reactstrap";
-import { connect } from "react-redux";
-import { searchMembers } from "../redux/actions/memberActions";
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { connect } from 'react-redux';
+import { searchMembers } from '../redux/actions/memberActions';
 import {
   ButtonDropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
-} from "reactstrap";
+} from 'reactstrap';
 
 class SearchFieldMember extends Component {
   constructor(props) {
     super(props);
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.props.dispatch(searchMembers(""));
+    this.props.dispatch(searchMembers(''));
     this.toggle = this.toggle.bind(this);
     this.state = {
       dropdownOpen: false
@@ -35,60 +35,62 @@ class SearchFieldMember extends Component {
       <Row className="search-field-member">
         <Col md="9" md={{ offset: 0, size: 6 }} sm={{ offset: 0 }}>
           <form className="search-form">
-            <img
-              className="search-icon"
-              src={require("../../public/img/search-grey.png")}
-              alt="Card i cap"
-            />
-            <input
-              className="search-input"
-              style={{ background: "none", border: "none" }}
-              placeholder="suchen..."
-              ref={input => (this.search = input)}
-              onChange={this.handleInputChange}
-            />
-            <ButtonDropdown
-              isOpen={this.state.dropdownOpen}
-              toggle={this.toggle}
-            >
-              <DropdownToggle
-                caret
-                className="filter-button"
-                color={"rgb(15, 25, 41, 40%)"}
+            <div className="testerSearch">
+              <img
+                className="search-icon"
+                src={require('../../public/img/search-grey.png')}
+                alt="Card i cap"
+              />
+              <input
+                className="search-input"
+                style={{ background: 'none', border: 'none' }}
+                placeholder="suchen..."
+                ref={input => (this.search = input)}
+                onChange={this.handleInputChange}
+              />
+              <ButtonDropdown
+                isOpen={this.state.dropdownOpen}
+                toggle={this.toggle}
               >
-                <svg
-                  className="filter-icon"
-                  viewBox="0 0 30 27"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
+                <DropdownToggle
+                  caret
+                  className="filter-button"
+                  color={'rgb(15, 25, 41, 40%)'}
                 >
-                  <path d="M17.5 12.0001L28 1.40341H15L2 1.40332L13 12.0001V26L17.5 23.0001V18.0001V12.0001Z" />
-                </svg>
-              </DropdownToggle>
+                  <svg
+                    className="filter-icon"
+                    viewBox="0 0 30 27"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                  >
+                    <path d="M17.5 12.0001L28 1.40341H15L2 1.40332L13 12.0001V26L17.5 23.0001V18.0001V12.0001Z" />
+                  </svg>
+                </DropdownToggle>
 
-              <DropdownMenu>
-                <DropdownItem header>Standorte filtern</DropdownItem>
+                <DropdownMenu>
+                  <DropdownItem header>Standorte filtern</DropdownItem>
 
-                <div className="drop-down-filter">
-                  <input type="checkbox" />
+                  <div className="drop-down-filter">
+                    <input type="checkbox" />
 
-                  <label className="filter-cities"> Bern</label>
-                </div>
+                    <label className="filter-cities"> Bern</label>
+                  </div>
 
-                <div className="drop-down-filter">
-                  <input type="checkbox" />
+                  <div className="drop-down-filter">
+                    <input type="checkbox" />
 
-                  <label className="filter-cities">Zürich</label>
-                </div>
+                    <label className="filter-cities">Zürich</label>
+                  </div>
 
-                <div className="drop-down-filter">
-                  <input type="checkbox" />
+                  <div className="drop-down-filter">
+                    <input type="checkbox" />
 
-                  <label className="filter-cities">St.Gallen</label>
-                </div>
-              </DropdownMenu>
-            </ButtonDropdown>
-            <hr />
+                    <label className="filter-cities">St.Gallen</label>
+                  </div>
+                </DropdownMenu>
+              </ButtonDropdown>
+              <hr />
+            </div>
           </form>
         </Col>
       </Row>
