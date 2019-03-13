12a3767b267149ab88eb31e3f21aa4a3879725e5
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2019-03-13 13:38:57 +0100

    VT341805-245: fixed test execution errors, created first test

--- client/src/redux/reducers/memberReducer.test.js
+++ client/src/redux/reducers/memberReducer.test.js
@@ -1,41 +1,57 @@
-import React from 'react';
+import store from '../../helpers/store';
+import * as actions from '../actions/memberActions';
+import axios from 'axios';
 
-//import filterMembers from './memberReducer';
-const memberReducer = require('./memberReducer');
+jest.mock('axios');
 
-const user1 = {
-  _id: '1',
-  firstname: 'Bruce',
-  surname: 'Wayne',
-  job: 'Banker',
-  function: 'CEO',
-  sector: 'Marketing',
-  company: 'Bruce Wayne Company',
-  circle: 'Bern'
-};
+const usersFromMockDB = [
+  {
+    _id: '1',
+    firstname: 'Bruce',
+    surname: 'Wayne',
+    job: 'Banker',
+    privateEmail: 'privateEmail',
+    privateTel: 'privateTel',
+    function: 'CEO',
+    sector: 'Marketing',
+    company: 'Bruce Wayne Company',
+    circle: 'Bern'
+  },
+  {
+    _id: '2',
+    firstname: 'Peter',
+    surname: 'Pan',
+    job: 'Pilot',
+    privateEmail: 'privateEmail',
+    privateTel: 'privateTel',
+    function: 'Piratenschreck',
+    sector: 'Fantasy',
+    company: 'PP Company',
+    circle: 'Zürich'
+  },
+  {
+    _id: '3',
+    firstname: 'Hans',
+    surname: 'Göthe',
+    job: 'Dichter',
+    privateEmail: 'privateEmail',
+    privateTel: 'privateTel',
+    function: 'Schreiber',
+    sector: 'Literatur',
+    company: 'Hansiverlag',
+    circle: 'Basel'
+  }
+];
 
-const user2 = {
-  _id: '2',
-  firstname: 'Peter',
-  surname: 'Pan',
-  job: 'Pilot',
-  function: 'Piratenschreck',
-  sector: 'Fantasy',
-  company: 'PP Company',
-  circle: 'Zürich'
-};
+describe('redux store member tests', () => {
+  it('loads the expected number of members on fetch action', done => {
+    const resp = { data: usersFromMockDB };
+    axios.get.mockResolvedValue(resp);
+    store.dispatch(actions.fetchMembers());
 
-const user3 = {
-  _id: '3',
-  firstname: 'Hans',
-  surname: 'Göthe',
-  job: 'Dichter',
-  function: 'Schreiber',
-  sector: 'Literatur',
-  company: 'Hansiverlag',
-  circle: 'Basel'
-};
-
-beforeEach(() => (members = [user1, user2, user3]));
-
-test('Filter Prename', () => {});
+    setTimeout(() => {
+      expect(store.getState().member.members.length).toEqual(3);
+      done();
+    }, 500);
+  });
+});
