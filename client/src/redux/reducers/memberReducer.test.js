aaf8155d6e911b4c14af4c5b10060d864fbd7425
Samuel Eiben <samuel.eiben@students.fhnw.ch>
2019-03-22 12:03:44 +0100

    VT341805-255: removed timeouts in tests where not needed

--- client/src/redux/reducers/memberReducer.test.js
+++ client/src/redux/reducers/memberReducer.test.js
@@ -98,170 +98,122 @@ describe('redux store member tests', () => {
 });
 
 describe('Test if the searchfilter filters the expected members', () => {
-  test('search for firstname Peter', done => {
+  test('search for firstname Peter', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'Peter');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(2);
-      expect(filteredMembers.includes(user2)).toBe(true);
-      expect(filteredMembers.includes(user4)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(2);
+    expect(filteredMembers.includes(user2)).toBe(true);
+    expect(filteredMembers.includes(user4)).toBe(true);
   });
 
-  test('search for surname Parker', done => {
+  test('search for surname Parker', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'Parker');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user4)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user4)).toBe(true);
   });
 
-  test('search for job pilot', done => {
+  test('search for job pilot', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'pilot');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user2)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user2)).toBe(true);
   });
 
-  test('search for sector marketing with 1 wrong char', done => {
+  test('search for sector marketing with 1 wrong char', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'mrketing');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user1)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user1)).toBe(true);
   });
 
-  test('search for sector marketing with 2 wrong chars', done => {
+  test('search for sector marketing with 2 wrong chars', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'metketing');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(0);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(0);
   });
 
-  test('search for company Hansiverlag with uncompleted searchtext', done => {
+  test('search for company Hansiverlag with uncompleted searchtext', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'Hansiverl');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
   });
 
-  test('search for function Schreiber with uncompleted searchtext and 1 wrong char', done => {
+  test('search for function Schreiber with uncompleted searchtext and 1 wrong char', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'schri');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user3)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user3)).toBe(true);
   });
 
-  test('searching with an empty searchtext', done => {
+  test('searching with an empty searchtext', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, '');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(usersFromMockDB.length);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(usersFromMockDB.length);
   });
 
-  test('searching for nonexistant entry', done => {
+  test('searching for nonexistant entry', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'jihdshfhgfdhüqsq');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(0);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(0);
   });
 
-  test('checking if umlauts are ignored', done => {
+  test('checking if umlauts are ignored', () => {
     let filteredMembers = usersFromMockDB;
     filteredMembers = filterMembers(filteredMembers, 'goethe');
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user3)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user3)).toBe(true);
   });
 });
 
 describe('Test if the circle filter filters the expected members', () => {
-  test('filters for expected members in Bern', done => {
+  test('filters for expected members in Bern', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['BE'];
     filteredMembers = filterByCircles(filteredMembers, checkedCircles);
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(2);
-      expect(filteredMembers.includes(user1)).toBe(true);
-      expect(filteredMembers.includes(user4)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(2);
+    expect(filteredMembers.includes(user1)).toBe(true);
+    expect(filteredMembers.includes(user4)).toBe(true);
   });
 
-  test('filters for expected members in Zürich', done => {
+  test('filters for expected members in Zürich', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['ZH'];
     filteredMembers = filterByCircles(filteredMembers, checkedCircles);
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user2)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user2)).toBe(true);
   });
 
-  test('filters for expected members in Basel', done => {
+  test('filters for expected members in Basel', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['BS'];
     filteredMembers = filterByCircles(filteredMembers, checkedCircles);
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user3)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user3)).toBe(true);
   });
 
-  test('filters for expected members in multiple cities', done => {
+  test('filters for expected members in multiple cities', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['ZH', 'BE', 'BS'];
     filteredMembers = filterByCircles(filteredMembers, checkedCircles);
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(usersFromMockDB.length);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(usersFromMockDB.length);
   });
 
-  test('filters for expected members in a non existant city', done => {
+  test('filters for expected members in a non existant city', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['SG'];
     filteredMembers = filterByCircles(filteredMembers, checkedCircles);
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(0);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(0);
   });
 
-  test('filters for expected members in a city already filtered by searchText', done => {
+  test('filters for expected members in a city already filtered by searchText', () => {
     let filteredMembers = usersFromMockDB;
     let checkedCircles = ['BE'];
     filteredMembers = filterMembers(
       filterByCircles(filteredMembers, checkedCircles),
       'Bruce'
     );
-    setTimeout(() => {
-      expect(filteredMembers.length).toEqual(1);
-      expect(filteredMembers.includes(user1)).toBe(true);
-      done();
-    }, 500);
+    expect(filteredMembers.length).toEqual(1);
+    expect(filteredMembers.includes(user1)).toBe(true);
   });
 });
