08a8a1a74388c479b0b9ea7bd27a9af4c22d033e
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2019-05-06 15:26:27 +0200

    VT341805-367: warning cleanup

--- client/src/components/NewsCard.js
+++ client/src/components/NewsCard.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Card, CardText, CardBody, CardTitle, Row, Col } from 'reactstrap';
-import { Link } from 'react-router-dom';
 
 class NewsCard extends Component {
   constructor(props) {
