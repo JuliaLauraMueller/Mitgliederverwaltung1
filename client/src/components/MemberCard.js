e1f197258be20b0b6292ee46b61a213a0754bf21
Benjamin Huber <benjamin.huber@students.fhnw.ch>
2019-05-09 09:54:47 +0200

    Revert "Merge branch 'feature/memberBugFix' into 'development'"
    This reverts merge request !61

--- client/src/components/MemberCard.js
+++ client/src/components/MemberCard.js
@@ -4,14 +4,6 @@ import { Link } from 'react-router-dom';
 
 class MemberCard extends Component {
   render() {
-    let trimmedEmail = this.props.member.privateEmail.substring(0, 22);
-    if (this.props.member.privateEmail.length > 22)
-      trimmedEmail = trimmedEmail + '...';
-    console.log(this.props.member.privateEmail);
-
-    let privateTelefon = this.props.member.privateTel;
-    console.log(privateTelefon);
-
     return (
       <Row className="member-card-row">
         <Col className="member-card-col">
@@ -44,9 +36,7 @@ class MemberCard extends Component {
               </div>
               <div>
                 <CardText className="card-text-mail">
-                  <a href={'mailto:' + this.props.member.privateEmail}>
-                    {trimmedEmail}
-                  </a>
+                  {this.props.member.privateEmail}
                 </CardText>
               </div>
               <div>
@@ -58,7 +48,7 @@ class MemberCard extends Component {
               </div>
               <div>
                 <CardTitle className="card-text-phone">
-                  <a href={'tel:' + privateTelefon}>{privateTelefon}</a>
+                  {this.props.member.privateTel}
                 </CardTitle>
               </div>
               <div className="profile-link">
@@ -68,7 +58,7 @@ class MemberCard extends Component {
                 >
                   <svg
                     className="profile-arrow"
-                    viewBox="0 0 7 12"
+                    viewBox="0 0 10 10"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M0.177999 1.02741L4.45449 5.91771L4.52644 6L4.45449 6.08229L0.177999 10.9726L1.19532 11.8255L5.9171 6.42454L5.91709 6.42454L5.91775 6.4238L6.29423 6L5.91775 5.5762L5.91775 5.57621L5.9171 5.57546L1.19532 0.174456L0.177999 1.02741Z" />
